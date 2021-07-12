e0eb5de57d6805d13abd5b3e02777b47d0780707
Maas Lalani <maaslalani0@gmail.com>
2021-07-12 16:34:28 -0400

    neogit

--- lua/nordbuddy/colors/neogit.lua
+++ lua/nordbuddy/colors/neogit.lua
@@ -5,6 +5,19 @@ return function(c)
         {'NeogitDiffDeleteHighlight', c.red, c.black},
         {'NeogitDiffContextHighlight', c.white, c.dark_black},
         {'NeogitHunkHeaderHighlight', c.grayish, c.dark_black},
-        {'NeogitHunkHeader', c.grayish, c.dark_black}
+        {'NeogitHunkHeader', c.grayish, c.dark_black},
+        {'NeogitFilePath', c.cyan},
+        {'NeogitUnmergedInto', c.blue},
+        {'NeogitUnpulledFrom', c.blue},
+        {'NeogitUntrackedfiles', c.blue},
+        {'NeogitUnstagedchanges', c.blue},
+        {'NeogitUnmergedchanges', c.blue},
+        {'NeogitUnpulledchanges', c.blue},
+        {'NeogitStagedchanges', c.cyan},
+        {'NeogitStashes', c.blue},
+        {'NeogitFold', c.grayish},
+        {'NeogitNotificationInfo', c.green},
+        {'NeogitNotificationWarning', c.yellow},
+        {'NeogitNotificationError', c.red},
     }
 end
